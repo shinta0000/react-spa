d2af7b42d0b991cff41e6ff8bab22a966ceac43e
ueno <uenoshinta0k@gmail.com>
2020-11-26 12:17:28 +0900

    add: Cardscomponentを表示

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import Header from './components/Header';
 import './App.css';
 import Main from './components/Main';
 import Footer from './components/Footer';
+import Cards from './components/Cards';
 
 function App() {
   return (
     <div className="App">
       <Header />
       <Main />
+      <Cards />
       <Footer />
     </div>
   );
